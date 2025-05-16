d6d7236e292ff8ea1d1a118bf54c620a79548d27
Nikhil Bhor <129065751+NikhilBBhor@users.noreply.github.com>
2025-05-16 14:40:43 +0000

    learned datatypes and variables

---
+++ 01_first_lesson/data-types.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(1 + 1) // not allowed since we are running it in browser
+
+// console.log('first line') console.log('second line')  // not allowed
+
+console.log(3   // not a good practice
+    + 
+    3
+);
+
+
+console.log('first line'); console.log('second line');  // allowed
+
+let name = "Nikhil" // String
+let age = 29 // Integer
+let isMarried = false // boolean
+
+console.log(typeof null);  // Object
+console.log(typeof undefined);  // Undefined
+
---
+++ 01_first_lesson/variables.js
@@ -0,0 +1,20 @@
+const accountId = 123456
+let accountEmail = "nikhilb.bhor@gmail.com"
+var accountPassword = "112244"
+accountCity = "Pune"
+let accountState
+
+// accountId = 654321 // not allowed
+
+accountEmail = "new@email.com"
+accountPassword = "554422"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+/*
+Do not use var
+Because it is deprecated
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
